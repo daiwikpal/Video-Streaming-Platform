32fac1c459fd367ae6c961151fb7d1e5ee7d9852
Daiwik Pal <daiwikpal123@gmail.com>
2024-02-10 17:06:10 -0500

    add signin/login starter code and connected firebase

--- web-client/app/firebase/firebase.ts
+++ web-client/app/firebase/firebase.ts
@@ -7,6 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+      apiKey: "AIzaSyDfdr2VGrQ6eJWuMy9Rnp0kZSZ1bqhr8kI",
+      authDomain: "video-streaming-platform-b0cda.firebaseapp.com",
+      projectId: "video-streaming-platform-b0cda",
+      storageBucket: "video-streaming-platform-b0cda.appspot.com",
+      messagingSenderId: "331679469986",
+      appId: "1:331679469986:web:600647d531fc35aa0ee2fb",
+      measurementId: "G-7G46J195D6"
 
 };
 
--- web-client/app/signin/page.tsx
+++ web-client/app/signin/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from 'react';
-
 import styles from './SignInPage.module.css'; 
 
 
